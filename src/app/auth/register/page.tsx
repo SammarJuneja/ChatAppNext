8d0533ee7e93b5e41b575c5bbbdfa8edc5d80d0b
KoolMoh <justmohammad66@gmail.com>
2024-06-18 12:01:48 +0300

    Add Login & Signup pages

---
+++ src/app/auth/register/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Signup = () => {
+  return (
+    <div className="app-bg">
+      <div className="log-tab p-5">
+        <input className="outline-none rounded-md my-2" type="text" />
+        <input className="outline-none rounded-md my-2" type="text" />
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
