b4346a1fd0e98c6752b422d0435a0d44ef0aba7a
SammarJuneja <sammarkumar345@gmail.com>
2024-06-18 17:33:45 +0530

    Add login page

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,14 +1,50 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+const apiUrl = "https://chat-api-kappa-tan.vercel.app";
 
-const Login = () => {
+const Login = async() => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [device, setDevice] = useState("");
+  const [error, setError] = useState("");
+
+  async function login() {
+    const response = await fetch(`${apiUrl}/auth/v1/login`, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+        device: device
+      }),
+    });
+    const data = await response.json();
+    if (response.ok) {
+      // router here
+      console.log(data)
+    } else {
+      setError(data.message)
+    }
+  }
   return (
-    <div className="app-bg">
-      <div className="log-tab p-5">
-        <input className="outline-none rounded-md my-2" type="text" />
-        <input className="outline-none rounded-md my-2" type="text" />
+    <div className="bg-app-bg items-center justify-center min-h-screen grid">
+      <div>
+        <p>{error}</p>
+        <div className="bg-log-tab grid text-gray-300 rounded-lg p-5">
+          <input value={email} onChange={(email) => setEmail(email.target.value)} className="outline-none bg-signup-button rounded-md my-2 p-2" type="text"  placeholder="Email"/>
+          <input value={password} onChange={(pass) => setPassword(pass.target.value)} className="outline-none bg-signup-button rounded-md my-2 p-2" type="password" placeholder="Password"/>
+        </div>
+        <div className="bg-login-button rounded-md p-2 mt-7 text-gray-300 text-center">
+          <button onClick={() => {
+            setDevice("I Phone 15")
+            login()
+          }}>Login</button>
+        </div>
       </div>
     </div>
-  );
+  ); 
 };
 
 export default Login;
